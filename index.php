7390ccc8dc3b4ad51fab5eb9e74d5b4ae1c5360e
rajkumar15971 <rajkumar.thadem772@gmail.com>
2016-06-30 11:16:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <head>
 </head>
 <body>
-<p><a href="hw1/hw1.html">Home work1</a></p>
-<p><a href="hw2/hw2.html">Home work2</a></p>
-<p><a href="hw3/hw3.html">Home work3</a></p>
-<p><a href="hw4/hw4.html">Home work4</a></p>
-<p><a href="hw5-1/hw5-1.html">Home work5 - 1</a></p>
-<p><a href="hw5-2/hw5-2.html">Home work5 - 2</a></p>
+<p><a href="hw1.html">Home work1</a></p>
+<p><a href="hw2.html">Home work2</a></p>
+<p><a href="hw3.html">Home work3</a></p>
+<p><a href="hw4.html">Home work4</a></p>
+<p><a href="hw5-1.html">Home work5 - 1</a></p>
+<p><a href="hw5-2.html">Home work5 - 2</a></p>
 </body>
 </html>
